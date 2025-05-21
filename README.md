79820f2f79f3c8de004b719e0c10d7302cc6c572
Ailton Cesar <ailtonc.n64@gmail.com>
2025-05-21 23:48:59 +0000

    Next React, Tailwind, favicon, title, README

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Portfolio-Js
+## Meu Portfolio - Ailton Santos
+
+# Tecnologias Usadas
+- Javascript com React Next como base.
+- Tailwind CSS para estilo e algumas dependências.
---
+++ portfolio/app/layout.js
@@ -0,0 +1,29 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Portfolio - Ailton Santos",
+  description: "Meu Portfolio utilizando Next React e suas dependências",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
