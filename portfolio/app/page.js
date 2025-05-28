fb224379fbb487a9c419d2b27df1b016ee1c988f
Ailton Cesar <ailtonc.n64@gmail.com>
2025-05-28 15:25:28 +0000

    ajuste de rota e novas pastas/arquivos

--- portfolio/app/page.js
+++ portfolio/app/page.js
@@ -1,25 +1,27 @@
 import Link from 'next/link';
 
 export default function Home() {
-  
+
   return (
     <main className="flex flex-col items-center justify-center h-screen gap-6">
-      <h1 className="text-4xl italic font-bold"> O que deseja fazer?</h1>
+      <h1 className="text-4xl italic font-bold">O que deseja fazer?</h1>
       <div className="flex flex-col items-center gap-4">
-        <Link href="/experienciaAcademica">
-          <button className="bg-purple-500 text-white px-4 py-2 rounded">Minha Experiência Acadêmica</button>
+        <Link href="/experienciaacademica">
+          <button className="bg-purple-500 text-white px-4 py-2 rounded">Experiência Acadêmica</button>
         </Link>
-        <Link href="/experienciaProfissional">
-          <button className="bg-purple-500 text-white px-4 py-2 rounded">Minha Experiência Profissional</button>
+        <Link href="/experienciaprofissional">
+          <button className="bg-purple-500 text-white px-4 py-2 rounded">Experiência Profissional</button>
         </Link>
-        <Link href="sobre">
+        <Link href="/projetosdesenvolvidos">
+          <button className="bg-purple-500 text-white px-4 py-2 rounded">Projetos Desenvolvidos</button>
+        </Link>
+        <Link href="/jogo">
+          <button className="bg-purple-500 text-white px-4 py-2 rounded">Bulls n' Cows</button>
+        </Link>
+        <Link href="/sobre">
           <button className="bg-purple-500 text-white px-4 py-2 rounded">Sobre o Projeto</button>
         </Link>
       </div>
     </main>
   );
 }
-
-/* precisa ajustar a troca de telas. 
-telas criadas mas mudanca não estão funcionando
-*/
